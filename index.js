2580802e1e4cb2d7f19d101d43ad44ecbf7e6928
Ajene <ajenec@gmail.com>
2024-10-08 11:01:49 -0400

    finish

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 //Write your solutions in this file, don't forget to test your functions.
+
+//1.
+const countFromOne = (num) => {
+  for (let i = 1; i <= num; i += 1) {
+    //would not work with return
+    console.log(i);
+  }
+};
+//countFromOne(13);
+
+//2
+const countEveryOdd = (num) => {
+  for (let i = 1; i <= num; i++) {
+    if (i % 2 !== 0) {
+      console.log(i);
+    }
+  }
+  return "done";
+};
+//countEveryOdd(55);
+
+//3
+const isNegative = (num) => {
+  // you can alse write it like this >>
+  return num < 0;
+  /*if (num < 0) {
+    return true;
+  } else {
+    return false;
+  }*/
+};
+console.log(isNegative(-2));
+
+//4
+const betweenFiveAndTwenty = (num) => {
+  if (num >= 5 && num <= 20) {
+    return true;
+  } else {
+    return false;
+  }
+};
+//console.log(betweenFiveAndTwenty(12));
+
+//5
+const sumOfThreeOrFive = () => {};
+
+//6
+const isAllLowerCase = () => {};
+
+//BONUS QUESTION
